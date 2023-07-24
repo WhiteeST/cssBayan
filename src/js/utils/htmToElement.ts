d2db0ccfcb37b20756ad990a88cb9a84c35a68c9
WhiteeST <whiteeluck123@gmail.com>
2023-07-24 19:45:04 +0500

    feat: deploy gh-pages

--- src/js/utils/htmToElement.ts
+++
@@ -1,5 +0,0 @@
-export default function (htmlString: string): ChildNode {
-  const template = document.createElement('template');
-  template.innerHTML = htmlString;
-  return <ChildNode>template.content.firstChild;
-}
